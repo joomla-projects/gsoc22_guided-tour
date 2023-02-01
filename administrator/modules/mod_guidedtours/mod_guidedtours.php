e6a6d80f8d89ab52dd02158f0b74d6f817cc69f9
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-01-31 19:30:34 -0500

    Removed script from module

--- administrator/modules/mod_guidedtours/mod_guidedtours.php
+++ administrator/modules/mod_guidedtours/mod_guidedtours.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\Plugin\PluginHelper;
-use Joomla\CMS\Uri\Uri;
 use Joomla\Module\GuidedTours\Administrator\Helper\GuidedToursHelper;
 
 if (!PluginHelper::isEnabled('system', 'tour')) {
@@ -24,25 +23,5 @@ $tours = GuidedToursHelper::getList($params);
 if (empty($tours)) {
     return;
 }
-
-$wa = $app->getDocument()->getWebAssetManager();
-$wa->addInlineScript('
-    function tourWasSelected(element) {
-        if (element.getAttribute("data-id") > 0) {
-            fetch("' . Uri::root() . 'administrator/index.php?option=com_ajax&plugin=tour&group=system&format=raw&method=post&tour_id=" + element.getAttribute("data-id"), {
-                method: "get"
-            })
-            .then(function(response) { return response.json(); })
-			.then(function(json) {
-                if (Object.keys(json).length > 0) {
-                    document.dispatchEvent(new CustomEvent("GuidedTourLoaded", { bubbles: true, detail: json }));
-                }
-            })
-            .catch(error => console.error(error));
-        } else {
-            console.log("no data-id");
-        }
-    }
-');
 
 require ModuleHelper::getLayoutPath('mod_guidedtours', $params->get('layout', 'default'));
