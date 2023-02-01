ec3e1c4713418dadeb63764aedbca58d0920a41c
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-01-31 19:32:51 -0500

    ESLint fixes Added translated keys

--- build/media_source/plg_system_tour/js/guide.es5.js
+++ build/media_source/plg_system_tour/js/guide.es5.js
@@ -4,36 +4,34 @@
  */
 
 function checkAndRedirect(redirectUrl) {
-  var currentURL = window.location.href;
-  if (currentURL != redirectUrl) {
+  const currentURL = window.location.href;
+  if (currentURL !== redirectUrl) {
     window.location.href = redirectUrl;
   }
 }
-function createTour() {
+
+function instantiateTour() {
   return new Shepherd.Tour({
     defaultStepOptions: {
-      scrollTo: true,
-      classes: "shadow",
       cancelIcon: {
         enabled: true,
       },
-      classes: "class-1 class-2 shepherd-theme-arrows",
-      scrollTo: { behavior: "smooth", block: "center" },
+      classes: 'class-1 class-2 shepherd-theme-arrows',
+      scrollTo: {
+        behavior: 'smooth',
+        block: 'center',
+      },
     },
     useModalOverlay: true,
     keyboardNavigation: true,
   });
 }
-function addCancelTourButton(tour) {
-  tour.on("cancel", () => {
-    sessionStorage.clear();
-  });
-}
-function addStepToTourButton(tour, obj, index, buttons, uri) {
+
+function addStepToTourButton(tour, obj, index, buttons) {
   tour.addStep({
     title: obj.steps[index].title,
     text: obj.steps[index].description,
-    classes: "intro-step shepherd-theme-arrows",
+    classes: 'intro-step shepherd-theme-arrows',
     attachTo: {
       element: obj.steps[index].target,
       on: obj.steps[index].position,
@@ -41,76 +39,88 @@ function addStepToTourButton(tour, obj, index, buttons, uri) {
       type: obj.steps[index].type,
       interactive_type: obj.steps[index].interactive_type,
     },
-
     buttons: buttons,
     id: obj.steps[index].id,
     arrow: true,
     showOn: obj.steps[index].position,
     when: {
       show() {
-        var currentstepIndex = `${tour.currentStep.id}` - "0";
-        sessionStorage.setItem("currentStepId", currentstepIndex);
-        if (obj.steps[index].type == 1) {
-          checkAndRedirect(uri + tour.currentStep.options.attachTo.url);
+        const currentStepIndex = `${tour.currentStep.id}`;
+        sessionStorage.setItem('currentStepId', String(currentStepIndex));
+        if (obj.steps[index].type === 1) {
+          checkAndRedirect(Joomla.getOptions('system.paths').rootFull + tour.currentStep.options.attachTo.url);
         }
       },
     },
   });
 }
+
 function addInitialStepToTourButton(tour, obj) {
   tour.addStep({
     title: obj.title,
     text: obj.description,
-    classes: "intro-step shepherd-theme-arrows",
+    classes: 'intro-step shepherd-theme-arrows',
     attachTo: {
-      on: "bottom",
+      on: 'bottom',
     },
     buttons: [
       {
         action() {
           return tour.next();
         },
-        text: "Start",
+        text: Joomla.Text._('PLG_SYSTEM_TOUR_START'),
       },
     ],
     id: obj.id,
   });
 }
+
+function addCancelTourEvent(tour) {
+  tour.on('cancel', () => {
+    sessionStorage.removeItem('currentStepId');
+    sessionStorage.removeItem('tourId');
+    const url = `${Joomla.getOptions('system.paths').rootFull}administrator/index.php?option=com_ajax&plugin=tour&group=system&format=raw&method=post&tour_id=-1`;
+    fetch(
+      url,
+      {
+        method: 'GET',
+      },
+    )
+      .catch((error) => console.error(error));
+    tour.steps = [];
+  });
+}
+
 function pushCompleteButton(buttons, tour) {
   buttons.push({
-    text: "Complete",
-    classes: "shepherd-button-primary",
+    text: Joomla.Text._('PLG_SYSTEM_TOUR_COMPLETE'),
+    classes: 'shepherd-button-primary',
     action: function () {
       return tour.cancel();
     },
   });
 }
-function pushNextButton(buttons, tour, step_id, disabled = false) {
+
+function pushNextButton(buttons, tour, stepId, disabled = false) {
   buttons.push({
-    text: "Next",
-    classes: `shepherd-button-primary step-next-button-${step_id}`,
+    text: Joomla.Text._('PLG_SYSTEM_TOUR_NEXT'),
+    classes: `shepherd-button-primary step-next-button-${stepId}`,
     action: function () {
       return tour.next();
     },
     disabled: disabled,
   });
 }
-function enableButton(e) {
-  const ele = document.querySelector(
-    `.step-next-button-${e.currentTarget.step_id}`
-  );
-  ele.removeAttribute("disabled");
-}
-function pushBackButton(buttons, tour, prev_step) {
+
+function pushBackButton(buttons, tour, prevStep) {
   buttons.push({
-    text: "Back",
-    classes: "shepherd-button-secondary",
+    text: Joomla.Text._('PLG_SYSTEM_TOUR_BACK'),
+    classes: 'shepherd-button-secondary',
     action: function () {
-      if (prev_step) {
-        const paths = Joomla.getOptions("system.paths");
-        sessionStorage.setItem("currentStepId", prev_step.id);
-        if (prev_step.type == 1) {
-          checkAndRedirect(paths.rootFull + prev_step.url);
+      if (prevStep) {
+        sessionStorage.setItem('currentStepId', prevStep.id);
+        if (prevStep.type === 1) {
+          checkAndRedirect(Joomla.getOptions('system.paths').rootFull + prevStep.url);
         }
       }
       return tour.back();
@@ -118,94 +128,124 @@ function pushBackButton(buttons, tour, prev_step) {
   });
 }
 
-function startTour(obj) {
+function enableButton(event) {
+  const element = document.querySelector(`.step-next-button-${event.currentTarget.step_id}`);
+  element.removeAttribute('disabled');
+}
 
-  const paths = Joomla.getOptions("system.paths");
-  const uri = paths.rootFull;
-
-  var currentStepId = sessionStorage.getItem("currentStepId");
-  let prev_step = "";
-  const tour = createTour();
-  var ind = 0;
+function CreateAndStartTour(obj) {
+  const currentStepId = sessionStorage.getItem('currentStepId');
+  let prevStep = '';
+  const tour = instantiateTour();
+  let ind = 0;
   if (currentStepId) {
-    ind = obj.steps.findIndex((x) => x.id == currentStepId);
+    ind = obj.steps.findIndex((x) => x.id === Number(currentStepId));
+    if (ind < 0) {
+      return;
+    }
     if (ind > 0) {
-      prev_step = obj.steps[ind - 1];
+      prevStep = obj.steps[ind - 1];
     }
   } else {
-    ind = 0;
     addInitialStepToTourButton(tour, obj);
   }
-  for (index = ind; index < obj.steps.length; index++) {
-    let buttons = [];
-    var len = obj.steps.length;
 
-    pushBackButton(buttons, tour, prev_step);
+  const len = obj.steps.length;
+  let buttons;
+
+  // eslint-disable-next-line no-plusplus
+  for (let index = ind; index < len; index++) {
+    buttons = [];
+
+    pushBackButton(buttons, tour, prevStep);
 
     if (
-      obj &&
-      obj.steps[index].target &&
-      obj.steps[index].type == 2
+      obj
+      && obj.steps[index].target
+      && obj.steps[index].type === 2
     ) {
       const ele = document.querySelector(obj.steps[index].target);
       if (ele) {
         if (obj && obj.steps[index].interactive_type === 2) {
           ele.step_id = index;
-          ele.addEventListener("input", enableButton, enableButton);
+          ele.addEventListener('input', enableButton, enableButton);
         }
         if (obj && obj.steps[index].interactive_type === 1) {
-          ele.addEventListener("click", tour.next, tour.next);
+          ele.addEventListener('click', tour.next, tour.next);
         }
       }
     }
 
-    if (index != len - 1) {
+    if (index !== len - 1) {
       let disabled = false;
-      if (obj && obj.steps[index].interactive_type == 2)
+      if (obj && obj.steps[index].interactive_type === 2) {
         disabled = true;
+      }
       if (
-        (obj && obj.steps[index].type !== 2) ||
-        (obj && obj.steps[index].interactive_type == 2) ||
-        (obj && obj.steps[index].interactive_type == 3)
-      )
+        (obj && obj.steps[index].type !== 2)
+        || (obj && obj.steps[index].interactive_type === 2)
+        || (obj && obj.steps[index].interactive_type === 3)
+      ) {
         pushNextButton(buttons, tour, index, disabled);
+      }
     } else {
       pushCompleteButton(buttons, tour);
     }
 
-    addStepToTourButton(tour, obj, index, buttons, uri);
-    prev_step = obj.steps[index];
+    addStepToTourButton(tour, obj, index, buttons);
+    prevStep = obj.steps[index];
   }
+  addCancelTourEvent(tour);
   tour.start();
-  addCancelTourButton(tour);
+}
+
+function tourWasSelected(element) {
+  if (element.getAttribute('data-id') > 0) {
+    const url = `${Joomla.getOptions('system.paths').rootFull}administrator/index.php?option=com_ajax&plugin=tour&group=system&format=raw&method=post&tour_id=${element.getAttribute('data-id')}`;
+    fetch(
+      url,
+      {
+        method: 'GET',
+      },
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        if (Object.keys(json).length > 0) {
+          document.dispatchEvent(new CustomEvent('GuidedTourLoaded', { bubbles: true, detail: json }));
+        }
+      })
+      .catch((error) => console.error(error));
+  } else {
+    console.log('tour: no data-id');
+  }
 }
 
 Joomla = window.Joomla || {};
-(function (Joomla, window) {
-  document.addEventListener("GuidedTourLoaded", function (e) {
 
-    sessionStorage.setItem("tourId", e.detail.id);
+((Joomla, document) => {
+  'use strict';
 
-    const paths = Joomla.getOptions("system.paths");
-    const uri = paths.rootFull;
-
-    var currentURL = window.location.href;
-    if (currentURL != uri + e.detail.url) {
-      window.location.href = uri + e.detail.url;
+  document.addEventListener('GuidedTourLoaded', (event) => {
+    sessionStorage.setItem('tourId', event.detail.id);
+    const uri = Joomla.getOptions('system.paths').rootFull;
+    const currentURL = window.location.href;
+    if (currentURL !== uri + event.detail.url) {
+      window.location.href = uri + event.detail.url;
     } else {
-      startTour(e.detail);
+      CreateAndStartTour(event.detail);
     }
   });
 
-  document.addEventListener("DOMContentLoaded", function (e) {
-
-    tourId = sessionStorage.getItem("tourId");
-
+  document.addEventListener('DOMContentLoaded', () => {
+    const tourId = sessionStorage.getItem('tourId');
     if (tourId) {
-      let myTour = Joomla.getOptions("myTour");
-      let obj = JSON.parse(myTour);
-
-      startTour(obj);
+      const myTour = Joomla.getOptions('myTour');
+      if (myTour) {
+        CreateAndStartTour(JSON.parse(myTour));
+      } else {
+        sessionStorage.removeItem('currentStepId');
+        sessionStorage.removeItem('tourId');
+      }
     }
   });
-})(Joomla, window);
+})(Joomla, document);
