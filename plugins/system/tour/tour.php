6046a7505fce20af020a0793293ce6dbd804f5bb
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-02-12 12:10:39 -0500

    Missing JEXEC or die

--- plugins/system/tour/tour.php
+++ plugins/system/tour/tour.php
@@ -14,6 +14,10 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Event\SubscriberInterface;
 
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
 /**
  * PlgSystemTour
  *
