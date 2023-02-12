0a7219ea7177619115446f039d3e8ebf8e7f3734
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-02-11 21:14:19 -0500

    Missing JEXEC or die

--- administrator/components/com_guidedtours/src/View/Steps/HtmlView.php
+++ administrator/components/com_guidedtours/src/View/Steps/HtmlView.php
@@ -20,6 +20,10 @@ use Joomla\CMS\Toolbar\ToolbarHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Guidedtours\Administrator\Helper\GuidedtoursHelper;
 
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
 /**
  * View class for a list of guidedtour_steps.
  *
